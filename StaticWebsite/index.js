dbc3f15efd98c5f87f45dd352dcbbc956ddc105b
veeraganesh944 <76054542+veeraganesh944@users.noreply.github.com>
2022-05-25 18:50:48 +0530

    Update index.js

--- StaticWebsite/index.js
+++ StaticWebsite/index.js
@@ -11,14 +11,13 @@ $(document).ready(function(){
         alert(title+" "+description+" "+completiondate+" "+imagename+" "+docname)
         $.ajax('https://fdpurq55gj.execute-api.ap-south-1.amazonaws.com/ProdTodo/tasks', {
             type: 'POST',  // http method
-            dataType: 'json',
-            //contentType: 'application/json',
+           // dataType: 'json',
             data: JSON.stringify({ id:Math.round(Math.random()*10000),title:title,description:description,completiondate:completiondate,imagename:imagename,docname:docname}),  // data to submit
             success: function (data, status, xhr) {
                 alert("uploaded Successfully")
                console.log($("#form1"))
-                 $( '#btn1' )[0].click()
-                 $( '#btn2' )[0].click()
+               $( '#btn1' )[0].click()
+               $( '#btn2' )[0].click()
 
              
             },
@@ -38,14 +37,12 @@ function( e ) {
     e.preventDefault();
     let form = $('#form1')[0]
   console.log('this is getting called')
-  
- $.ajax( {
-    url: 'http://3.110.84.186:8080/upload-image',
+  $.ajax( {
+    url: 'http://65.1.108.118:5000/upload-image',
     type: 'POST',
-    crossDomain:true,
     data: new FormData( form ),
     processData: false,
-    contentType: "multipart/form-data",
+    contentType: false,
     success:function(){
         console.log("file successfully uploaded")
     },
@@ -60,12 +57,11 @@ $( '#btn2' ).click( function( e ) {
     e.preventDefault();
     let form = $('#form2')[0]
   $.ajax( {
-    url: 'http://3.110.84.186:8080/upload-image',
+    url: 'http://65.1.108.118:5000/upload-image',
     type: 'POST',
-    crossDomain:true,
     data: new FormData( form ),
     processData: false,
-    contentType: "multipart/form-data",
+    contentType: false,
     success:function(){
         console.log("file successfully uploaded")
     },
@@ -84,8 +80,8 @@ function readData(){
            code+="<h3>"+data.tasks[x].title+"</h3>"
            code+="<p>"+data.tasks[x].description+"</p>"
            code+="<h6>Completion Date:"+data.tasks[x].completiondate+"</h6>"
-           code+="<h5> Document :: <a href='http://3.110.84.186:8080/download-file/"+data.tasks[x].docname+"'>"+data.tasks[x].docname+"</a><h5>"
-           code+="<h5>Image::<h5>"+"<img src='http://3.110.84.186:8080/download-file/"+data.tasks[x].imagename+"'>"
+           code+="<h5> Document :: <a href='http://65.1.108.118:5000/download-file/"+data.tasks[x].docname+"'>"+data.tasks[x].docname+"</a><h5>"
+           code+="<h5>Image::<h5>"+"<img src='http://65.1.108.118:5000/download-file/"+data.tasks[x].imagename+"'>"
            code+="</li>"
         }
         code+="</ul>"
